e656a16f99d27aa94c38dca3c482d6b6383ed595
Enrique Hernandez <enrique.hz.t@gmail.com>
2020-11-07 18:38:41 -0600

    Ready to test

--- src/PokeNameValidator.js
+++ src/PokeNameValidator.js
@@ -1,20 +1,40 @@
 import React, { useState, useEffect } from "react";
 import pokemons from "./pokemons";
 
+let randomPokemon = pokemons[Math.floor(Math.random() * pokemons.length)];
+
 export default function PokeNameValidator() {
   /**
    * Component logic goes here
    */
+
+  //console.log(randomPokemon);
+  const [pokeName, setName] = useState("");
+  //console.log(pokeName);
+
+  if (randomPokemon.toLowerCase() === pokeName.toLowerCase()) {
+    alert("✔️✔️Correct!!!✔️✔️  Do you want to play again?");
+    randomPokemon = pokemons[Math.floor(Math.random() * pokemons.length)];
+    document.getElementById("pokeInput").value = "";
+    //location.reload();
+  }
 
   return (
     <div className="app">
       <h2>Mini Challenge 4: Poke Name Validator</h2>
       <p>
         Pokémon name:&nbsp;
-        <strong>{/** random pokemon name should appear here */}</strong>
+        <strong>{randomPokemon}</strong>
       </p>
       <p className="secondary">Type the Pokémon name correctly:</p>
-      <input type="text" placeholder="Enter the Pokémon name" />
+      <input
+        id="pokeInput"
+        type="text"
+        placeholder="Enter the Pokémon name"
+        onChange={(event) => {
+          setName(event.target.value);
+        }}
+      />
     </div>
   );
 }
